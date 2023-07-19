cbf6899a69f3e1d4cd80a16d29b8daecc12e784a
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-19 08:23:51 -0500

    se crea proyecto sql con folders y files

---
+++ leagues/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Query usando SQL de este repositorio de las ligas de futbol del mundo',
+    author='dasxgo',
+    license='',
+)
